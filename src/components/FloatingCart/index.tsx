55fb64eae2779a8a52d66631e92651763f1677ca
leandrovi <leandrofernandesvieira@gmail.com>
2020-05-22 09:59:17 -0300

    feat: floating cart calcs

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,24 +15,23 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products
+      .map(product => product.price * product.quantity)
+      .reduce((sum, next) => sum + next);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.length;
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
